c83fdd52cadee7a21fa4b67d5d64ad5f28722da0
Walter Prince <wprince0288@gmail.com>
2025-01-08 15:58:11 -0600

    basic server started and httpie working

--- api/server.js
+++ api/server.js
@@ -1,3 +1,18 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
+// const User = require('./model.js')
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express()
+
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message: 'not found' 
+    })
+})
+
+//ENDPOINTS
+
+
+
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const server = require('./api/server');
 const port = 9000;
 
 // START YOUR SERVER HERE
+
+server.listen(port, () => {
+    console.log(`server started on http://localhost:9000`)
+})
