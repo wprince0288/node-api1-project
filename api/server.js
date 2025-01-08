f35ec706aae577b30900aa35bc138e18597fb067
Walter Prince <wprince0288@gmail.com>
2025-01-08 17:16:57 -0600

    post endpoint working

--- api/server.js
+++ api/server.js
@@ -2,6 +2,28 @@
 const express = require('express')
 const User = require('./users/model')
 const server = express()
+server.use(express.json())
+
+server.post('/api/users', (req, res) => {
+    const user = req.body;
+    if (!user.name || !user.bio) {
+        res.status(400).json({
+            message: "Please provide name and bio for the user",
+        })
+    } else {
+        User.insert(user)
+            .then(createdUser => {
+                res.status(201).json(createdUser)
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: 'error creating user',
+                    err: err.message,
+                    stack: err.stack,
+                })
+            })
+    }
+})
 
 server.get('/api/users', (req, res) => {
     User.find()
