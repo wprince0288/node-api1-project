ddd1f81297df2fc6a876b9c1bb99e892328e5478
Walter Prince <wprince0288@gmail.com>
2025-01-08 18:04:56 -0600

    all tests passing

--- api/server.js
+++ api/server.js
@@ -4,15 +4,25 @@ const User = require('./users/model')
 const server = express()
 server.use(express.json())
 
+//ENDPOINTS
+
 server.delete('/api/users/:id', async (req, res) => {
-    const possibleUser = await User.findById(req.params.id)
-    if (!possibleUser) {
-        res.status(404).json({
-            message: "The user with the specified ID does not exist",
+    try {
+        const possibleUser = await User.findById(req.params.id)
+        if (!possibleUser) {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist",
+            })
+        } else {
+            const deletedUser = await User.remove(req.params.id)
+            res.status(200).json(deletedUser)
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: 'error creating user',
+            err: err.message,
+            stack: err.stack,
         })
-    } else {
-        const deletedUser = await User.remove(req.params.id)
-        res.status(200).json(deletedUser)
     }
 })
 
@@ -69,13 +79,38 @@ server.get('/api/users/:id', (req, res) => {
         })
 })
 
+server.put('/api/users/:id', async (req, res) => {
+    try {
+        const possibleUser = await User.findById(req.params.id)
+        if (!possibleUser) {
+            res.status(404).json({
+                message: 'The user with the specified ID does not exist',
+            })
+        } else {
+            if (!req.body.name || !req.body.bio) {
+                res.status(400).json({
+                    message: 'Please provide name and bio for the user',
+                })
+            } else {
+                const updatedUser = await User.update(req.params.id, req.body,)
+                res.status(200).json(updatedUser)
+            }
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: 'error updating user',
+            err: err.message,
+            stack: err.stack,
+        })
+    }
+})
+
 server.use('*', (req, res) => {
     res.status(404).json({
         message: 'not found'
     })
 })
 
-//ENDPOINTS
 
 
 module.exports = server; 
